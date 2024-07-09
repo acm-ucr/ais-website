51ca97166469a6367c9cae028412e7f00471f120
Howard Zhu <67174263+Howard-Z@users.noreply.github.com>
2024-07-09 10:45:05 -0700

    Intial implementation

--- src/components/Value.jsx
+++ src/components/Value.jsx
@@ -1,7 +1,30 @@
 import React from "react";
+import { values } from "@/data/cards.js";
 
-const Value = () => {
-  return <div>Value</div>;
+// selector takes in a string that maps to a dictionary entry
+// the dictionary can be found in src/data/cards
+// each entry contains the backgroudn color, the text, the title, and img path
+const Value = ({ selector }) => {
+  const color = values[selector]["color"];
+  console.log(color)
+  const baseClass = `rounded-lg text-white font-nunito bg-[${color}]
+                    flex flex-col items-center
+                    max-w-[380px] mx-auto p-4 gap-3`;
+  // const base_class = `rounded-lg font-nunito flex flex-col items-center max-w-[380px] mx-auto p-4 gap-3 bg-[${color}]`;
+  // const base_class = `rounded-lg text-white font-nunito flex flex-col items-center max-w-[380px] mx-auto p-4 gap-3 bg-[#051d40]`;
+
+
+  return (
+    <div className={baseClass}>
+      <img className="" src={values[selector]["img"]} />
+      <div className="font-extrabold text-[36px] text-center tracking-wide">
+        {values[selector]["title"]}
+      </div>
+      <div className="cont-semibold text-[24px] text-center max-w-[308px]">
+        {values[selector]["text"]}
+      </div>
+    </div>
+  );
 };
 
 export default Value;
---
+++ src/data/cards.js
@@ -0,0 +1,20 @@
+export const values = {
+  education: {
+    color: "#051d40",
+    title: "Education",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.",
+    img: "value_icons/computer.svg",
+  },
+  innovation: {
+    title: "Innovation",
+    color: "#ca064d",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.",
+    img: "value_icons/graph-up.svg",
+  },
+  community: {
+    title: "Community",
+    color: "#051d40",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.",
+    img: "value_icons/people.svg",
+  },
+};
