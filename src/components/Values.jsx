55797f41158f3a5dabeab444c99214dd537d9a80
Howard Zhu <67174263+Howard-Z@users.noreply.github.com>
2024-07-09 11:42:01 -0700

    added update for Values.jsx

--- src/components/Values.jsx
+++ src/components/Values.jsx
@@ -1,17 +1,15 @@
-import React from 'react'
-import Value from './Value';
+import React from "react";
+import Value from "./Value";
 
 const Values = () => {
-    const cards = ["education", "innovation", "community"];
+  const cards = ["education", "innovation", "community"];
   return (
-    <div className='flex gap-10 justify-center'>
-        {
-            cards.map((x) => (
-            <Value selector={x}/>
-        ))
-        }
+    <div className="flex gap-10 justify-center">
+      {cards.map((x, index) => (
+        <Value selector={x} key={index} />
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default Values
+export default Values;
