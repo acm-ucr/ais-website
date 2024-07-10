13ce6cf6fc08fedacc718cc09b4c757cc0f9856d
Tanya <tanya.carrillo707@gmail.com>
2024-07-10 00:11:33 -0700

    added the aboutLanding.svg to the AboutLanding.jsx along with ABOUT US text

---
+++ src/components/AboutLanding.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const AboutLanding = () => {
+  return (
+    <div>
+      AboutLanding
+      <img src="aboutLanding.svg"></img>
+      <p style={{ textAlign: "center" }}> ABOUT US </p>
+    </div>
+  );
+};
+
+export default AboutLanding;
