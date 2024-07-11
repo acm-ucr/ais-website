48a327e64d2128d8edf35962cd08235a058b26f3
Kavin Phabiani <kavinphab@gmail.com>
2024-07-10 22:17:41 -0700

    centered content

--- src/components/Placement.jsx
+++ src/components/Placement.jsx
@@ -5,28 +5,30 @@ import Image from "next/image";
 const Placement = () => {
   return (
     <div>
-      <div className="grid grid-rows-2 grid-flow-col justify-center gap-8">
-        <div className="text-xl">Our Placements</div>
-        <div className="bg-green-500">Check out where we work</div>
+      <div className="text-2xl h-1/5 flex justify-center">Our Placements</div>
+      <div className="bg-green-500 text-xl flex justify-center">
+        Check out where we work
       </div>
-      <div className="grid grid-cols-3 grid-rows-2 bg-red-500 justify-items-center w-full h-1/3 gap-2 place-self-center mx-14">
-        <div className="h-1/2 w-1/2">
-          <Image src={Sony} alt="Sony" />
-        </div>
-        <div className="h-1/2 w-1/2">
-          <Image src={Sony} alt="Sony" />
-        </div>
-        <div className="h-1/2 w-1/2">
-          <Image src={Sony} alt="Sony" />
-        </div>
-        <div className="h-1/2 w-1/2">
-          <Image src={Sony} alt="Sony" />
-        </div>
-        <div className="h-1/2 w-1/2">
-          <Image src={Sony} alt="Sony" />
-        </div>
-        <div className="h-1/2 w-1/2">
-          <Image src={Sony} alt="Sony" />
+      <div className="w-[90%] h-full flex flex-col my-3 items-center mx-auto gap-8">
+        <div className="grid grid-cols-3 justify-items-center justify-center gap-8">
+          <div className="h-1/2 w-1/2">
+            <Image src={Sony} alt="Sony" />
+          </div>
+          <div className="h-1/2 w-1/2">
+            <Image src={Sony} alt="Sony" />
+          </div>
+          <div className="h-1/2 w-1/2">
+            <Image src={Sony} alt="Sony" />
+          </div>
+          <div className="h-1/2 w-1/2">
+            <Image src={Sony} alt="Sony" />
+          </div>
+          <div className="h-1/2 w-1/2">
+            <Image src={Sony} alt="Sony" />
+          </div>
+          <div className="h-1/2 w-1/2">
+            <Image src={Sony} alt="Sony" />
+          </div>
         </div>
       </div>
     </div>
