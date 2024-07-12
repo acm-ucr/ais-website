1c7090427399662b19197a63ff2e36ddfe7ca209
stormyy00 <jonathan.trujillo0432@gmail.com>
2024-07-12 14:42:15 -0700

    updated dev

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,13 @@
 import Join from "@/components/Join";
 import Landing from "@/components/Landing";
+import Placement from "@/components/Placement";
 
 const Home = () => {
   return (
     <div>
       <Landing />
       <Join />
+      <Placement />
     </div>
   );
 };
