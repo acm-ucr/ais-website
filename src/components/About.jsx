24308a707fe5d23957269e48dead90947890812d
Abhi Tamatam <abhitamatam1@gmail.com>
2024-07-12 23:28:32 -0700

    new changes

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,25 +1,38 @@
 import React from "react";
 import Image from "next/image";
-import Kitten from "@/public/Kitten.png";
+import GroupPhoto from "@/public/AIS_About_GroupPhoto.png";
 
 const About = () => {
   return (
-    <div className="max-w-7x1 mx-auto py-12">
-      <h1 className="text-center font-semibold text-4x1 mb-6">About AIS</h1>
-      <div className="relative grid grid-cols-2">
+    <div className="max-w-7x1 mx-auto py-12 px-10">
+      <div className="grid grid-cols-2 items-start gap-8">
         <div className="relative">
-          <Image
-            src={Kitten}
-            alt="Placeholder for Image"
-            width={1000}
-            height={667}
-          />
+          <div className="absolute bottom-4 right-4 bg-blue-100 rounded-2xl w-full h-full"></div>
+          <div className="relative rounded-2x1">
+            <Image
+              src={GroupPhoto}
+              alt="AIS Group Photo"
+              layout="responsive"
+              width={4096}
+              height={2730}
+              className="rounded-2xl"
+            />
+          </div>
         </div>
-
-        <p className="max-w-prose" py-12 px-4>
-          We are the Association for Information Systems (AIS) at the University
-          of California, Riverside...
-        </p>
+        <div>
+          <h1 className="text-4xl font-semibold mb-6">About AIS</h1>
+          <p className="text-lg">
+            We are the Association for Information Systems (AIS) at the
+            University of California, Riverside. We are a student-run
+            organization that aims to develop and promote professional and
+            technical skills that are valuable to members interested in
+            information systems, data analysis, and other related fields. With
+            weekly meetings, we support our members with opportunities and
+            workshops for networking, relevant certifications, information
+            systems conferences, professional and personal development, and
+            exposure among working professionals, guest speakers, and peers.
+          </p>
+        </div>
       </div>
     </div>
   );
