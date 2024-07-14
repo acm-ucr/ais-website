c6609762edaf37505e01eab94515972aa856f5dd
Tanya <tanya.carrillo707@gmail.com>
2024-07-14 16:50:30 -0700

    fixed the gradient and now added the text atop the image for the about us landing page

--- src/components/AboutLanding.jsx
+++ src/components/AboutLanding.jsx
@@ -1,11 +1,16 @@
 import React from "react";
+import Image from "next/image";
+import aboutLanding from "@/public/aboutLanding.svg";
 
 const AboutLanding = () => {
   return (
-    <div>
-      AboutLanding
-      <img src="aboutLanding.svg"></img>
-      <p style={{ textAlign: "center" }}> ABOUT US </p>
+    <div className="relative bg-gradient-to-b from-ais-blue via-[#04125F]  to-[#01057F]">
+      <div className="absolute text-4xl font-bold font-nunito text-white top-[50%] left-[46%]">
+        <p>ABOUT US</p>
+      </div>
+      <div>
+        <Image className="w-full" src={aboutLanding} />
+      </div>
     </div>
   );
 };
