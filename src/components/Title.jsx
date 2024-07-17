1eb7e87a2be1eea1b7d498b5623385ad4b6898bd
manishgandi24 <mgand014@ucr.edu>
2024-07-16 18:13:51 -0700

    title

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,21 +1,6 @@
 import React from "react";
 
-// const Title = ({ title }) => {
-//   return <div>{title}</div>;
-// };
-
-// export default Title;
 const Title = ({ title }) => {
-  return (
-    <div className="flex-col inline-flex gap-3 items-center justify-center">
-      <div className="text-art-pink-100  text-2xl md:text-4xl p-1 font-bold">
-        {title}
-        <div className="flex justify-center">
-          <div className="mt-1 w-12 h-1.5 bg-art-pink-100 rounded-lg" />
-        </div>
-      </div>
-    </div>
-  );
+  return <div className="text-2xl text-ais-blue-100">{title}</div>;
 };
-
 export default Title;
