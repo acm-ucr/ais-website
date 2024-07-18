a0188db25fc46c424152a5ad33d6176d9f146594
Marcus Hsieh <80995617+MarcusHsieh@users.noreply.github.com>
2024-07-17 20:27:02 -0700

    Make Offer component, card, and data

--- src/components/calendar/Offer.jsx
+++ src/components/calendar/Offer.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import Title from "../Title";
+import Card from "./OfferCard";
+import { VALUES } from "@/data/offer";
 
 const Offer = () => {
-  return <div>Offer</div>;
+  return (
+    <div className="flex flex-col my-3 gap-5 justify-center font-nunito items-center">
+      <Title title={"WHAT WE OFFER"} />
+      <div className="flex justify-center gap-5 w-full items-start">
+        {VALUES.map((item, index) => (
+          <Card
+            image={item.img}
+            title={item.title}
+            text={item.text}
+            key={index}
+          />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Offer;
---
+++ src/components/calendar/OfferCard.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import Image from "next/image";
+
+const OfferCard = ({ image, title, text }) => {
+  return (
+    <div
+      className={`flex flex-col items-center w-[330px] h-auto mx-5 p-2 gap-3 rounded-2xl text-black font-nunito`}
+    >
+      <div className="h-56 w-full relative">
+        <Image
+          src={image}
+          layout="fill"
+          className="mt-2 rounded-2xl object-cover"
+          alt="offer"
+        />
+      </div>
+      <div className="font-bold text-2xl text-center tracking-wide whitespace-nowrap mt-5">
+        {title}
+      </div>
+      <div className="cont-semibold text-l text-center w-full -mt-2">
+        {text}
+      </div>
+    </div>
+  );
+};
+
+export default OfferCard;
