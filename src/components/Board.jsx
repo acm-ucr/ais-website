ace57f48e5b08e80a5945560d388044112c5030a
Kavin Phabiani <kavinphab@gmail.com>
2024-07-20 13:29:37 -0700

    fixed spacing removed green background

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -6,13 +6,13 @@ import { FaLinkedinIn } from "react-icons/fa";
 
 const Board = () => {
   return (
-    <div className="bg-green-500 w-1/6 rounded-xl border border-black">
+    <div className="w-1/6 rounded-xl border border-black">
       <Image src={Cat} alt="cat" />
       <div className="flex flex-col items-center justify-center">
         <div>Justin Lamy</div>
         <div>President</div>
       </div>
-      <div className="flex flex-row justify-center gap-2">
+      <div className="flex flex-row justify-center gap-2 mb-4">
         <IoMdMail />
         <FaLinkedinIn />
       </div>
