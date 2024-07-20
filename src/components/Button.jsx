4dd601ee50df0a0d60f2d235b554a4cdfa037734
Jasmine Trình <168297703+minnieland@users.noreply.github.com>
2024-07-19 19:20:33 -0700

    changed code to have no variable names & transparent bg button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,17 +2,13 @@ import React from "react";
 import Link from "next/link";
 
 const Button = ({ text, link }) => {
-  const baseStyle =
-    "rounded-full text-[2rem] font-plain text-center inline-block";
-  const joinUsStyle =
-    "border-[3px] border-white bg-[#030E68] text-white py-[10px] px-[30px] ";
-  const learnMoreStyle =
-    "border-[3px] border-[#021260] bg-white text-[#021260] py-[10px] px-[30px]";
-
   return (
     <button
-      className={`${baseStyle} ${
-        text === "JOIN US" ? joinUsStyle : learnMoreStyle
+      className={`rounded-full text-[2rem] font-plain text-center inline-block
+      ${
+        text === "JOIN US"
+          ? "border-[3px] border-white bg-transparent text-white py-[10px] px-[30px] "
+          : "border-[3px] border-[#021260] bg-white text-[#021260] py-[10px] px-[30px]"
       }`}
     >
       <Link href={link} className="inline-block w-full h-full">
