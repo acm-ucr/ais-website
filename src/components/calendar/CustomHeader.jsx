20c2750f63a5d3718f73898a89671a4e0044e873
sunbrandon <sunabrandon@gmail.com>
2024-07-30 10:44:46 -0700

    Added index.css to calendar

--- src/components/calendar/CustomHeader.jsx
+++ src/components/calendar/CustomHeader.jsx
@@ -1,6 +1,8 @@
+import "./index.css";
+
 const Header = ({ label }) => {
   return (
-    <div className="pt-2 h-12 rounded-full font-medium md:text-2xl text-xs bg-ais-blue-200 text-white">
+    <div className="pt-2 h-12 rounded-full font-medium md:text-2xl text-xs bg-ais-blue-200 text-white flex items-center justify-center">
       {label}
     </div>
   );
