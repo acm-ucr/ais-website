4caf12e928fe6509428a16ae8c04a2e47b9454a4
Jasmine Trình <168297703+minnieland@users.noreply.github.com>
2024-08-01 00:26:48 -0700

    board format without image

--- src/components/board/Intern.jsx
+++ src/components/board/Intern.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import { IoMdMail } from "react-icons/io";
+import { FaLinkedinIn } from "react-icons/fa";
 
 const Intern = () => {
-  return <div>Intern</div>;
+  return (
+    <div className="w-1/6 rounded-xl border border-black">
+      <div className="flex flex-col items-center justify-center">
+        <div>Justin Lamy</div>
+        <div>President</div>
+      </div>
+      <div className="flex flex-row justify-center gap-2 mb-4">
+        <IoMdMail />
+        <FaLinkedinIn />
+      </div>
+    </div>
+  );
 };
 
 export default Intern;
