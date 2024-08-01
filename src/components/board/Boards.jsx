020feaac674b2ac164aaf4b9446beb985c58aedb
stormyy00 <jonathan.trujillo0432@gmail.com>
2024-08-01 00:43:51 -0700

    small change

--- src/components/board/Boards.jsx
+++ src/components/board/Boards.jsx
@@ -3,17 +3,31 @@ import Board from "./Board";
 import { VALUES } from "@/data/board";
 const Boards = () => {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 justify-items-center gap-y-14">
-      {VALUES.map((item, index) => (
-        <Board
-          key={index}
-          image={item.img}
-          name={item.name}
-          position={item.pos}
-          email={item.email}
-          linkedin={item.linkedin}
-        />
-      ))}
+    <div className="flex flex-col gap-y-14">
+      <div className="flex justify-around">
+        {VALUES.slice(0, 2).map((item, index) => (
+          <Board
+            image={item.img}
+            name={item.name}
+            position={item.pos}
+            email={item.email}
+            linkedin={item.linkedin}
+            key={index}
+          />
+        ))}
+      </div>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 justify-items-center gap-y-14">
+        {VALUES.slice(2).map((item, index) => (
+          <Board
+            key={index}
+            image={item.img}
+            name={item.name}
+            position={item.pos}
+            email={item.email}
+            linkedin={item.linkedin}
+          />
+        ))}
+      </div>
     </div>
   );
 };
