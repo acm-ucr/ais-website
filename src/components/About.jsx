72459f232e6e7d9515ee69ade33a635b3439d6ec
Jasmine Trình <168297703+minnieland@users.noreply.github.com>
2024-08-07 13:22:09 -0700

    responsive about.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,18 +5,18 @@ import Title from "./Title";
 
 const About = () => {
   return (
-    <div className="flex flex-col lg:flex-row mx-10 py-12 px-10 gap-10 justify-center font-nunito">
+    <div className="flex flex-col lg:flex-row mx-10 py-12 sm:px-10 gap-10 justify-center font-nunito items-center lg:items-start">
       <Image
         src={GroupPhoto}
         alt="AIS Group Photo"
         className="w-5/12 shadow-[-17px_-17px_0px_0px_#dce6f9] rounded-2xl mr-[17px] object-cover h-auto"
       />
 
-      <div className="flex flex-col justify-center space-y-6 mr-12 w-2/3 py-5">
-        <div className="flex justify-start">
+      <div className="flex flex-col justify-center space-y-6 mr-12 lg:w-2/3 py-5 text-left">
+        <div className="flex justify-center lg:justify-start">
           <Title title={"ABOUT US"} />
         </div>
-        <div className="text-left text-lg my-4">
+        <div className="text-left text-base sm:text-lg my-4">
           We are the Association for Information Systems (AIS) at the University
           of California, Riverside. We are a student-run organization that aims
           to develop and promote professional and technical skills that are
