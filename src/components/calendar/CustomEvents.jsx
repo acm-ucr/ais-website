725b8039f654e2f479c7475914d351db91573f95
sunbrandon <sunabrandon@gmail.com>
2024-08-07 17:24:30 -0700

    Created custom events

--- src/components/calendar/CustomEvents.jsx
+++ src/components/calendar/CustomEvents.jsx
@@ -1,6 +1,6 @@
 const CustomEvent = ({ event }) => {
   return (
-    <div className="border-[6.5px] border-ais-blue-200 rounded-full py-0 md:py-[1px] drop-shadow-[1px_1px_0px_rgba(191,140,225,.70)]">
+    <div className="border-[2px] border-ais-blue-200 rounded-full py-0 md:py-[1px]">
       <div className=" py-.5 rounded-full font-playfair text-sm flex justify-start p-1 text-black overflow-hidden">
         <p className="whitespace-nowrap m-0">
           {!event.allDay &&
--- src/components/calendar/Modal.jsx
+++ src/components/calendar/Modal.jsx
@@ -3,17 +3,13 @@ import reactHtmlParser from "html-react-parser";
 
 const Modal = ({ event, setEvent }) => {
   return (
-    <div className="font-nunito fixed top-[56%] left-1/2 -translate-x-1/2 -translate-y-1/2 w-[75vw] md:w-[50vw] z-10">
+    <div className="font-nunito fixed top-[56%] left-1/2 -translate-x-1/2 -translate-y-1/2 w-[75vw] md:w-[50vw] z-10 bg-ais-blue-200 text-white rounded-xl">
       <div className={`flex justify-between items-center ${event.color}`}>
-        <p
-          className={
-            " m-0 py-2 md:py-3 px-3 md:px-4 text-lg md:text-2xl  text-white"
-          }
-        >
+        <p className={" m-0 py-2 md:py-3 px-3 md:px-4 text-lg md:text-2xl"}>
           {event.summary}
         </p>
 
-        <div className="absolute right-2 md:py-3 px-12 md:px-12 text-lg md:text-2xl text-white">
+        <div className="absolute right-2 md:py-3 px-12 md:px-12 text-lg md:text-2xl">
           {event.allDay
             ? "All Day"
             : new Date(event.start).toLocaleTimeString(navigator.language, {
@@ -29,13 +25,13 @@ const Modal = ({ event, setEvent }) => {
 
         <AiOutlinePlus
           onClick={() => setEvent(null)}
-          className="text-white rotate-45 p-0 hover:scale-110 duration-300 hover:cursor-pointer text-3xl m-3"
+          className="rotate-45 p-0 hover:scale-110 duration-300 hover:cursor-pointer text-3xl m-3"
         />
       </div>
       <div className="p-3">
-        <p className="m-0 pl-2 text-white">{event.location}</p>
+        <p className="m-0 pl-2">{event.location}</p>
 
-        <div className="md:text-lg text-sm p-2 text-white">
+        <div className="md:text-lg text-sm p-2">
           {event.description ? reactHtmlParser(event.description) : " "}
         </div>
       </div>
