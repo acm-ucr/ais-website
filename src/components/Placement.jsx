72d7bb304bf1cb0598ba28730c894ce5e6c75f99
Zami <zalam007@ucr.edu>
2024-08-13 14:49:35 -0700

    w-change

--- src/components/Placement.jsx
+++ src/components/Placement.jsx
@@ -7,15 +7,14 @@ const Placement = () => {
   return (
     <div className="flex flex-col w-full items-center">
       <div className="h-1 border-0 bg-black w-1/12 rounded-full mb-3" />
-
       <div className="text-3xl h-1/5 flex justify-center font-nunito">
         <Title title={"Our Placements"} />
       </div>
       <div className="text-2xl flex justify-center mb-9 font-nunito italic">
         Check out where we work
       </div>
-
-      <div className="grid grid-cols-2 md:grid-cols-3 w-full md:w-9/12 items-center justify-center gap-4">
+      ``
+      <div className="grid grid-cols-2 md:grid-cols-3 w-full md:w-10/12 lg:w-9/12 items-center justify-center gap-4">
         {Placements.map((item, index) => (
           <div
             key={index}
