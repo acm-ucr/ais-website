3cd02bf3e73fcdb441bc44555cfe3fef8cecf3a3
Howard Zhu <67174263+Howard-Z@users.noreply.github.com>
2024-08-14 04:15:41 -0700

    added logic

--- src/components/calendar/Past.jsx
+++ src/components/calendar/Past.jsx
@@ -1,5 +1,4 @@
-import React from "react";
-import { PAST_EVENTS } from "@/data/past";
+import React, { useEffect, useState } from "react";
 
 const Event = ({ name, date, location }) => {
   return (
@@ -15,21 +14,102 @@ const Event = ({ name, date, location }) => {
   );
 };
 
+function pastFilter(item) {
+  const date = new Date();
+  const d = new Date(item.end.dateTime);
+  return d < date;
+}
+
+function formatDate(dateString) {
+  const date = new Date(dateString);
+  const daysOfWeek = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+
+  const dayOfWeek = daysOfWeek[date.getUTCDay()];
+  const month = months[date.getUTCMonth()];
+  const day = date.getUTCDate();
+
+  // Add suffix to the day (e.g., "1st", "2nd", "3rd", "4th", etc.)
+  const daySuffix = (day) => {
+    if (day > 3 && day < 21) return `${day}th`;
+    switch (day % 10) {
+      case 1:
+        return `${day}st`;
+      case 2:
+        return `${day}nd`;
+      case 3:
+        return `${day}rd`;
+      default:
+        return `${day}th`;
+    }
+  };
+
+  return `${dayOfWeek} ${month} ${daySuffix(day)}`;
+}
+
 const Past = () => {
+  const [data, setData] = useState(null);
+  const [isLoading, setLoading] = useState(true);
+  let filteredDates;
+
+  useEffect(() => {
+    fetch(
+      "https://www.googleapis.com/calendar/v3/calendars/jonathan.trujillo0432@gmail.com/events?key=AIzaSyDjMfOhKeS4EwrdI3muVh6tB_GiJTj1sAA"
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        setData(data);
+        setLoading(false);
+      });
+  }, []);
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+  if (!data) {
+    return <p>No data</p>;
+  }
+  if (data) {
+    console.log("This is data: ", data.items);
+    filteredDates = data.items.filter((item, index) => pastFilter(item));
+  }
+
   return (
     <div className="">
       <div className="ml-[270px] justify-center w-full font-nunito text-xl font-extrabold">
         2023-2024
       </div>
       <div className="flex justify-center gap-24 w-full">
-        {PAST_EVENTS.map((item, index) => (
-          <Event
-            name={item.event_name}
-            date={item.date}
-            location={item.location}
-            key={index}
-          />
-        ))}
+        {filteredDates
+          .slice(filteredDates.length - 3, filteredDates.length)
+          .map((item, index) => (
+            <Event
+              name={item.summary}
+              date={formatDate(item.start.dateTime)}
+              location={item.location}
+              key={index}
+            />
+          ))}
       </div>
     </div>
   );
