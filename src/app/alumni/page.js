5faabf0ddc1ad97b1c9cba49609e417c5bf2f501
stormyy00 <jonathan.trujillo0432@gmail.com>
2024-08-13 23:44:28 -0700

    add

--- src/app/alumni/page.js
+++ src/app/alumni/page.js
@@ -1,9 +1,11 @@
 import AboutLanding from "@/components//about/AboutLanding";
+import Placement from "@/components/Placement";
 
 const page = () => {
   return (
     <div>
       <AboutLanding header={"OUR ALUMNI"} />
+      <Placement />
     </div>
   );
 };
