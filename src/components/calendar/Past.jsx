d130f1fe7327b36695629bcfd5cc32c7e6612579
stormyy00 <jonathan.trujillo0432@gmail.com>
2024-08-14 04:56:56 -0700

    clean up howard chen zhu's mess

--- src/components/calendar/Past.jsx
+++ src/components/calendar/Past.jsx
@@ -1,11 +1,11 @@
 import React, { useEffect, useState } from "react";
-
-const Event = ({ name, date, location }) => {
+import moment from "moment";
+const Event = ({ name, month, day, location }) => {
   return (
     <div className="font-nunito border rounded-lg border-black shadow-sm shadow-black py-2.5">
       <div className="font-extrabold text-xl text-center pt-1 px-2">{name}</div>
       <div className="font-semibold text-base text-center pt-1 px-2">
-        {date} |{" "}
+        {month} {day}
       </div>
       <div className="font-semibold text-base text-center pt-1 px-2">
         {location}
@@ -14,101 +14,53 @@ const Event = ({ name, date, location }) => {
   );
 };
 
-function pastFilter(item) {
-  const date = new Date();
-  const d = new Date(item.end.dateTime);
-  return d < date;
-}
-
-function formatDate(dateString) {
-  const date = new Date(dateString);
-  const daysOfWeek = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-  ];
-  const months = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December",
-  ];
-
-  const dayOfWeek = daysOfWeek[date.getUTCDay()];
-  const month = months[date.getUTCMonth()];
-  const day = date.getUTCDate();
-
-  // Add suffix to the day (e.g., "1st", "2nd", "3rd", "4th", etc.)
-  const daySuffix = (day) => {
-    if (day > 3 && day < 21) return `${day}th`;
-    switch (day % 10) {
-      case 1:
-        return `${day}st`;
-      case 2:
-        return `${day}nd`;
-      case 3:
-        return `${day}rd`;
-      default:
-        return `${day}th`;
-    }
-  };
-
-  return `${dayOfWeek} ${month} ${daySuffix(day)}`;
-}
-
 const Past = () => {
-  const [data, setData] = useState(null);
-  const [isLoading, setLoading] = useState(true);
-  let filteredDates;
+  const [data, setData] = useState([]);
 
   useEffect(() => {
+    const startDate = moment().subtract(10, "weeks").toISOString();
+    const endDate = moment().add(10, "weeks").toISOString();
     fetch(
-      `https://www.googleapis.com/calendar/v3/calendars/${process.env.NEXT_PUBLIC_GOOGLE_CALENDAR_EMAIL}/events?key=${process.env.NEXT_PUBLIC_GOOGLE_CALENDAR_API_KEY}`
+      `https://www.googleapis.com/calendar/v3/calendars/${process.env.NEXT_PUBLIC_GOOGLE_CALENDAR_EMAIL}/events?key=${process.env.NEXT_PUBLIC_GOOGLE_CALENDAR_API_KEY}&singleEvents=true&orderBy=starttime&timeMin=${startDate}&timeMax=${endDate}&maxResults=10`
     )
       .then((res) => res.json())
       .then((data) => {
-        setData(data);
-        setLoading(false);
+        const events = data.items.map((a) => {
+          a.start = a.start.dateTime
+            ? new Date(a.start.dateTime)
+            : new Date(a.start.date);
+          a.end = a.end.dateTime
+            ? new Date(a.end.dateTime)
+            : new Date(a.end.date);
+          a.isAllDay = !a.start.dateTime;
+          return a;
+        });
+
+        setData(events);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
       });
   }, []);
-  if (isLoading) {
-    return <p>Loading...</p>;
-  }
-  if (!data) {
-    return <p>No data</p>;
-  }
-  if (data) {
-    filteredDates = data.items.filter((item, index) => pastFilter(item));
-  }
+
+  const past = data.filter((e) => e.start <= new Date());
 
   return (
-    <div className="">
-      <div className="ml-[270px] justify-center w-full font-nunito text-xl font-extrabold">
-        2023-2024
-      </div>
-      <div className="flex justify-center gap-24 w-full">
-        {filteredDates
-          .slice(filteredDates.length - 3, filteredDates.length)
-          .map((item, index) => (
-            <Event
-              name={item.summary}
-              date={formatDate(item.start.dateTime)}
-              location={item.location}
-              key={index}
-            />
-          ))}
+    <div className="flex justify-center w-full font-nunito text-xl font-extrabold">
+      <div className="grid justify-center gap-10 grid-cols-3 w-8/12">
+        {past.slice(-6).map((item, index) => (
+          <Event
+            name={item.summary}
+            month={item.start
+              .toLocaleDateString("en-US", { month: "short" })
+              .toUpperCase()}
+            day={item.start.toLocaleDateString("en-US", {
+              day: "2-digit",
+            })}
+            location={item.location}
+            key={index}
+          />
+        ))}
       </div>
     </div>
   );
