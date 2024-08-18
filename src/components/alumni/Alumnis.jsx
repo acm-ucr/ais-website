8ac00b56341aeff65c98cb9be2a655bfba229e20
stormyy00 <jonathan.trujillo0432@gmail.com>
2024-08-18 03:38:56 -0700

    rm map

--- src/components/alumni/Alumnis.jsx
+++ src/components/alumni/Alumnis.jsx
@@ -6,23 +6,12 @@ const Alumnis = () => {
   return (
     <div className="flex flex-col gap-y-14">
       <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 justify-items-center gap-y-14">
-        {CHARACTERISTICS.slice(0, 9).map((alumni, index) => (
+        {CHARACTERISTICS.map((alumni, index) => (
           <Alumni
             key={index}
             image={alumni.image}
             name={alumni.name}
             position={alumni.position}
-            linkedin={alumni.linkedin}
-          />
-        ))}
-      </div>
-      <div className="flex justify-center">
-        {CHARACTERISTICS.slice(9).map((alumni, index) => (
-          <Alumni
-            key={index}
-            image={alumni.image}
-            name={alumni.name}
-            title={alumni.position}
             linkedin={alumni.linkedin}
           />
         ))}
