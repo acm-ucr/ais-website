796a560393045007dc1e96c59aee2d7e09ae2db8
Zami <zalam007@ucr.edu>
2024-08-20 20:19:25 -0700

    added framer motion animations to board

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,6 +1,7 @@
 "use client";
 import React, { useState } from "react";
 import Title from "./Title";
+import { motion } from "framer-motion";
 
 const Filter = ({ filters, onChange, title, className }) => {
   const [selectedFilter, setSelectedFilter] = useState(filters[0]);
@@ -8,22 +9,24 @@ const Filter = ({ filters, onChange, title, className }) => {
     setSelectedFilter(filter);
     onChange(filter);
   };
+
   return (
     <div className="m-0 md:ml-6">
       <Title title={title} />
       <div className="flex space-x-4 mb-5 mt-4">
         {filters.map((filter) => (
-          <button
+          <motion.button
             key={filter}
             className={`w-48 px-6 py-1.5 rounded-full font-nunito ${
               selectedFilter === filter
                 ? "bg-ais-blue-100 text-white"
                 : "border-2 border-ais-blue-100 text-ais-blue-100"
             }`}
+            whileHover={{ scale: 1.05 }}
             onClick={() => handleClick(filter)}
           >
             {filter.charAt(0).toUpperCase() + filter.slice(1)}
-          </button>
+          </motion.button>
         ))}
       </div>
     </div>
